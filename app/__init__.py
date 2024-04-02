9039ae7d702e192f3b5b53d1f585c35da8bd7f65
mbgoodguy <nerooren4001@gmail.com>
2024-04-02 06:35:50 +0300

    Add 'pythonpath = . app' for test running through terminal

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,23 @@
-if __name__ == '__main__':
+from fastapi import APIRouter
+from app.views.products_views import router as products_router
+from app.views.user_views import router as users_router
 
-    from fastapi import APIRouter
-    from app.views.products_views import router as products_router
-    from app.views.user_views import router as users_router
+from app.demo_auth.views import router as demo_auth_router
+from app.demo_auth.demo_jwt_auth import router as jwt_router
 
-    from app.demo_auth.views import router as demo_auth_router
-    from app.demo_auth.demo_jwt_auth import router as jwt_router
+from app.homeworks.homework_3_3 import router as homework_3_3_router
+from app.homeworks.homework_4_1 import router as homework_4_1_router
+from app.homeworks.homework_6_2 import router as homework_6_2_router
 
-    from app.homeworks.homework_3_3 import router as homework_3_3_router
-    from app.homeworks.homework_4_1 import router as homework_4_1_router
-    from app.homeworks.homework_6_2 import router as homework_6_2_router
+router = APIRouter()  # подключить в main.py
 
-    router = APIRouter()  # подключить в main.py
+router.include_router(router=demo_auth_router)  # префикс не указываем т.к он уже указан в самом роутере
+router.include_router(router=products_router)
+router.include_router(router=users_router)
+router.include_router(router=jwt_router)
 
-    router.include_router(router=demo_auth_router)  # префикс не указываем т.к он уже указан в самом роутере
-    router.include_router(router=products_router)
-    router.include_router(router=users_router)
-    router.include_router(router=jwt_router)
+router.include_router(router=homework_3_3_router)
+router.include_router(router=homework_4_1_router)
+router.include_router(router=homework_6_2_router)
 
-    router.include_router(router=homework_3_3_router)
-    router.include_router(router=homework_4_1_router)
-    router.include_router(router=homework_6_2_router)
+
