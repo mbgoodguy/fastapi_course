ed71416d0ea459fba60f99057e715626607c0501
mbgoodguy <nerooren4001@gmail.com>
2024-04-16 11:10:06 +0300

    Add UserService, UsersRepository and create_user endpoint

--- app/homeworks/homework_7_3.py
+++ app/homeworks/homework_7_3.py
@@ -1,42 +1,67 @@
+from typing import Optional
+
 import uvicorn
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
-from sqlalchemy import create_engine, Integer, String, CheckConstraint
-from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column, Session
+from sqlalchemy import create_engine, Integer, String, CheckConstraint, insert, Column
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Session
 
 from db.config import settings
 
 app = FastAPI()
 engine = create_engine(url=settings.DB_URL)
-session = sessionmaker(bind=engine, expire_on_commit=False)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 
 class Base(DeclarativeBase):
     pass
 
-
 class User(Base):
     __tablename__ = 'user'
-    id: Mapped[int] = mapped_column(Integer(), primary_key=True)
-    username: Mapped[str] = mapped_column(String(length=20), unique=True)
-    age: Mapped[int] = mapped_column(Integer(), CheckConstraint('age > 0 and age < 100'))
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(length=20), unique=True, index=True)
+    age = Column(Integer(), CheckConstraint('age > 0 and age < 100'))
 
 
-def get_db():
-    with session() as db:
-        yield db
-
+class UsersRepository:
+    @classmethod
+    def add(cls, db: Session, values: dict):
+        stmt = insert(User).values(**values).returning(User)
+        new_user = db.execute(stmt)
+        db.commit()
+        return new_user.scalar_one()
 
 class UserPayload(BaseModel):
     username: str
     age: int
 
+class UserService:
+    @classmethod
+    def add(cls, db: Session, user: UserPayload):
+        new_user = UsersRepository.add(db, user.model_dump())
+        return new_user
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.post('/user')
 def create_user(data: UserPayload, db: Session = Depends(get_db)):
-    db.add(data)
-    db.commit()
+    try:
+        UserService.add(db=db, user=data)
+        return data
+    except Exception as e:
+        raise HTTPException(
+            status_code=400,
+            detail=str(e)
+        )
 
 
 if __name__ == '__main__':
-    uvicorn.run('app.homework_7_3:app', reload=True)
+    uvicorn.run('app.homeworks.homework_7_3:app', reload=True)
